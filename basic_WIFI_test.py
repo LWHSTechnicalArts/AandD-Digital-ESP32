8ac8f344ccece8d7d535457b8804e9cfa69a0e49
Andrew Kleindolph <akleindolph@gmail.com>
2026-07-23 20:32:48 -0700

    Create basic_WIFI_test.py

---
+++ basic_WIFI_test.py
@@ -0,0 +1,6 @@
+import os
+import wifi
+
+print(f"Connecting to {os.getenv('CIRCUITPY_WIFI_SSID')}")
+wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD"))
+print(f"Connected! IP address: {wifi.radio.ipv4_address}")
