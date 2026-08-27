197c3e58e01f6212f7a64cc4fd04ffd43a9cbbd0
Andrew Kleindolph <akleindolph@gmail.com>
2026-08-27 12:00:53 -0700

    Update test_w_comments.py

--- test_w_comments.py
+++ test_w_comments.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Unlicense
+# Created and Verified: A Kleindolph, August 2026 — tested on Feather ESP32-S3 with Circuitpy v10
+
 import time  # gives us access to time.sleep() for delays
 
 while True:  # loop forever - this keeps running until the board is reset or code changes
